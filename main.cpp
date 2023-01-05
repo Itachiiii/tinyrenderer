e3fb11c59769436e67e102a5f8204a8c309d0b95
zhouchen <857720484@qq.com>
2023-01-05 11:33:41 +0800

    lesson2: line sweeping [part 1]

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-const int width = 800;
-const int height = 800;
+const TGAColor green = TGAColor(0, 255,   0,   255);
+const int width = 200;
+const int height = 200;
 Model* model = nullptr;
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
@@ -52,34 +53,26 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     }
 }
 
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color){
+    line(t0.x, t0.y, t1.x, t1.y, image, color);
+    line(t1.x, t1.y, t2.x, t2.y, image, color);
+    line(t2.x, t2.y, t0.x, t0.y, image, color);
+}
+
 int main(int argc, char** argv) {
     TGAImage image(width, height, TGAImage::RGB);
-    if(2 == argc)
-    {
-        model = new Model(argv[1]);
-    }
-    else
-    {
-        model = new Model("obj/african_head.obj");
-    }
-    int fs = model->nfaces();
-    for(int f = 0; f < fs; f++)
-    {
-        auto vs = model->face(f);
-        for(int l = 0; l < 3; l++)
-        {
-            Vec3f v0 = model->vert(vs[l]);
-            Vec3f v1 = model->vert(vs[(l+1)%3]);
-            v0.x = (v0.x + 1) * width / 2;
-            v0.y = (v0.y + 1) * height / 2;
-            v1.x = (v1.x + 1) * width / 2;
-            v1.y = (v1.y + 1) * height / 2;
-            line(v0.x, v0.y, v1.x, v1.y, image, white);
-        }
-    }
+    
+    Vec2i t0[3] = {Vec2i(10, 70),   Vec2i(50, 160),  Vec2i(70, 80)};
+    Vec2i t1[3] = {Vec2i(180, 50),  Vec2i(150, 1),   Vec2i(70, 180)};
+    Vec2i t2[3] = {Vec2i(180, 150), Vec2i(120, 160), Vec2i(130, 180)};
+
+    triangle(t0[0], t0[1], t0[2], image, red);
+    triangle(t1[0], t1[1], t1[2], image, white);
+    triangle(t2[0], t2[1], t2[2], image, green);
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     delete model;
     return 0;
 }
+
